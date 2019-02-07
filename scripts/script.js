0ddc7c7c465e5bf8e2b45ff99471cc9be8d30c82
Prasanna Ashok Viswanathan <v.prasannaashok@gmail.com>
2019-02-08 03:31:39 +0530

    few mode upgrades on MyContacts-beta
    added "Delete All" button, Contacts as Table, Refresh link,

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,32 @@
 this.database = firebase.database();
 let contactsRef = this.database.ref("contacts");
 
-// UI Objects
+// UI Objects 
 var txtName = document.getElementById("txtName");
 var txtMobile = document.getElementById("txtMobile");
+var lnkUpdate = document.getElementById("lnkUpdate");
+var lblUpdatedStatus = document.getElementById("lblUpdatedStatus");
+var lblUpdateAlert = document.getElementById("lblUpdateAlert");
+
+
+// Gobals
+var updatedContacts = {}
+var firstRefreshOver = false;
 
 function saveUpdateContact(contact){
     contactsRef.child(contact.id).set(contact)
+}
+
+function deleteAllContacts(){
+    var res = confirm("Do you want to delete all Contacts?");
+    if(res){
+        contactsRef.set({});
+        updateContactsList();
+    }
+}
+
+function deleteContact(contact_id){
+    contactsRef.child(contact_id).set({})
 }
 
 function createContactObj(nameVal, mobileVal){
@@ -33,23 +53,106 @@ function addContact(){
     alert("New Contact created!")
 }
 
-document.getElementById("btnAdd").onclick = addContact;
-
 function updateContactsList(){
     
-    var lst = document.getElementById("lstContacts");
-    while (lst.firstChild) {
-        lst.removeChild(lst.firstChild);
+    // updating table
+
+    // clean old rows
+    var tbl = document.getElementById("tblContacts");
+    while(tbl.firstChild){
+        tbl.removeChild(tbl.firstChild);
     }
-    contactsRef.on('value', function(contacts){
-        contacts.forEach(function(contact){
+
+    if(updatedContacts.val() != null){
+        // header
+        tbl.appendChild(createContactsHeaderRow());
             
-            var li = document.createElement("li");
-            li.appendChild(document.createTextNode(contact.val().name + ", " + contact.val().mobile));
-            lst.appendChild(li);
+        // rows
+        var i=1;
+        updatedContacts.forEach(function(contact){
+            tbl.appendChild(createNewContactRow(i++,contact.val().name, contact.val().mobile));
         });
-    });
+    }
+
+    
+    
+    show(lblUpdatedStatus);
+    hide(lblUpdateAlert);
+}
+
+function alertOnUpdate(contacts){
+    updatedContacts = contacts;
+    if(!firstRefreshOver){
+        updateContactsList();
+        firstRefreshOver = true;
+    }
+    else{
+        hide(lblUpdatedStatus);
+        show(lblUpdateAlert);
+    }
     
 }
 
-updateContactsList();
+// --- Events ---
+
+document.getElementById("btnAdd").onclick = addContact;
+
+contactsRef.on('value', function(contacts){
+    alertOnUpdate(contacts);
+});
+
+lnkUpdate.onclick = function(){
+    updateContactsList();
+}
+
+document.getElementById("btnDeleteAll").onclick = function(){
+    deleteAllContacts();
+}
+
+// ----- basic methods ------
+
+function hide(el){
+    el.style.display = "none"
+}
+
+function show(el){
+    el.style.display = "block"
+}
+
+function createContactsHeaderRow(){
+    var tr = document.createElement("tr");
+
+    var td0 = document.createElement("th");
+    td0.appendChild(document.createTextNode("SNo"));
+    tr.appendChild(td0);
+        
+    var td1 = document.createElement("th");
+    td1.appendChild(document.createTextNode("Name"));
+    tr.appendChild(td1);
+
+    var td2= document.createElement("th");
+    td2.appendChild(document.createTextNode("Mobile No"));
+    tr.appendChild(td2);
+
+    return tr;
+}
+
+function createNewContactRow(sno,name,mobile){
+    var tr = document.createElement("tr");
+
+    var td0 = document.createElement("td");
+    td0.appendChild(document.createTextNode(sno));
+    tr.appendChild(td0);
+        
+    var td1 = document.createElement("td");
+    td1.appendChild(document.createTextNode(name));
+    tr.appendChild(td1);
+
+    var td2= document.createElement("td");
+    td2.appendChild(document.createTextNode(mobile));
+    tr.appendChild(td2);
+
+    return tr;
+}
+
+
